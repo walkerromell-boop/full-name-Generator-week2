55af77c85e3c746dc96b47cefa31053d4a9c774a
Romell Walker <walkerromell@gmail.com>
2025-09-30 14:21:15 -0400

    Finished ex 1

---
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First name: ");
+        String firstname = scanner.nextLine().trim();
+        System.out.println("Middle name: ");
+        String middlename = scanner.nextLine().trim();
+        System.out.println("Last name: ");
+        String lastname = scanner.nextLine().trim();
+        System.out.println("Suffix: ");
+        String suffix = scanner.nextLine().trim();
+
+        String fullname = firstname;
+        if (!middlename.equals(" ")) {
+            fullname = fullname + " " + middlename;
+        }
+        if (!suffix.equals(" ")){
+            fullname=fullname+" "+ " "+lastname+" "+suffix;
+        }
+        System.out.println(fullname);
+
+
+    }
+}
