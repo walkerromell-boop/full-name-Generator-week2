985ad9622623e3d35202d34d178be728dc829fe6
Romell Walker <walkerromell@gmail.com>
2025-09-30 11:38:52 -0400

    Inital commit

---
+++ src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Hello {
+    public static void main(String[] args) {
+        String welcome = "Little man says\"Howdy\"!!!";
+        System.out.println(welcome);
+
+        String welcome2= "This is a message.\n It never ends, or does it.\n Stay tuned and find out...";
+        System.out.println(welcome2);
+        String welcome3= """
+                Hey how is it going""";
+        System.out.println(welcome3);
+    }
+}
